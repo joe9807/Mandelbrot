bd29a05db8af9a2553e9da19e06899af97990ce3
joe9807 <joe9807@gmail.com>
2022-08-30 22:11:42 +0300

    get time elapsed

--- mandelbrot/src/mandelbrot/Mandelbrot.java
+++ mandelbrot/src/mandelbrot/Mandelbrot.java
@@ -1,5 +1,7 @@
 package mandelbrot;
 
+import java.util.Date;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
@@ -57,7 +59,9 @@ public class Mandelbrot {
 		canvas.addPaintListener(new PaintListener() {
 			@Override
 			public void paintControl(PaintEvent e) {
+				Date date = new Date();
 				mandelbrot(e.gc);
+				System.out.println(getTimeElapsed(new Date().getTime()-date.getTime()));
 			}
 		});
 	}
@@ -112,5 +116,42 @@ public class Mandelbrot {
 	
 	private double module(double x, double y) {
 		return x*x+y*y;
+	}
+	
+	private String getTimeElapsed(long elapsed) {
+        long milliseconds = elapsed % 1000;
+        elapsed = elapsed / 1000;
+        long seconds = elapsed % 60;
+        elapsed = elapsed / 60;
+        long minutes = elapsed % 60;
+        elapsed = elapsed / 60;
+        long hours = elapsed % 24;
+        elapsed = elapsed / 24;
+        long days = elapsed % 7;
+        elapsed = elapsed / 7;
+        long weeks = elapsed % 4;
+        elapsed = elapsed / 4;
+        long months = elapsed % 12;
+        long years = elapsed / 12;
+        
+        String millisStr = (milliseconds != 0? (milliseconds + " ms") : "");
+		String secondsStr = (seconds != 0 ? (seconds + " s ") : "");
+		String minutesStr = (minutes != 0 ? (minutes + " m ") : "");
+		String hoursStr = (hours != 0 ? (hours + " h ") : "");
+		String daysStr = (days != 0 ? (days + " d ") : "");
+		String weeksStr = (weeks != 0 ? (weeks + " w ") : "");
+		String monthsStr = (months != 0 ? (months + " M ") : "");
+		String yearsStr = (years != 0 ? (years + " y ") : "");
+		
+		String result = new StringBuilder(yearsStr)
+			.append(monthsStr)
+			.append(weeksStr)
+			.append(daysStr)
+			.append(hoursStr)
+			.append(minutesStr)
+			.append(secondsStr)
+			.append(millisStr).toString();
+		
+		return result.isEmpty()?"0 ms":result;
 	}
 }
